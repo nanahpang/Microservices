cd4c8e84e414be7f3793e19ccd36c58f8b31f05e
nanahpang <nanahpang@gmail.com>
2018-05-03 16:41:31 -0700

    eventuate final deliverable, delete v1,v2,v3,v4

--- eventuate/v2/common/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/common/shipment/ShipmentEvent.java
+++
@@ -1,9 +0,0 @@
-package net.chrisrichardson.eventstore.examples.customersandorders.common.shipment;
-
-
-import io.eventuate.Event;
-import io.eventuate.EventEntity;
-
-@EventEntity(entity = "net.chrisrichardson.eventstore.examples.customersandorders.shipmentservice.backend.Shipment")
-public interface ShipmentEvent extends Event {
-}
--- eventuate/v2/shipment-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/shipmentservice/backend/ShipmentCommand.java
+++
@@ -1,6 +0,0 @@
-package net.chrisrichardson.eventstore.examples.customersandorders.shipmentservice.backend;
-
-import io.eventuate.Command;
-
-public interface ShipmentCommand extends Command {
-}
